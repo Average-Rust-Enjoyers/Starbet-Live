0799386cfe8266907068552697d64b345bc4f80c
Adam Valt <valt.adam@gmail.com>
2023-11-15 17:51:16 +0100

    feat: add basic actix server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().route(
+            "/",
+            web::get().to(|| async { HttpResponse::Ok().body("/") }),
+        )
+    }).bind(("127.0.0.1", 6969))?.run().await
 }
