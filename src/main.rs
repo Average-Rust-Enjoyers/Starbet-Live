2f8ac4a445efddef759aa4d9361ece1917092f90
Adam Valt <valt.adam@gmail.com>
2023-11-15 18:58:18 +0100

    Add another bad formated code to test pipeline

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,7 @@ use actix_web::{web,
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new().route(
-            "/",
-            web::get().to(|| async { HttpResponse::Ok().body("/") }),
+            "/",web::get().to(|| async { HttpResponse::Ok().body("/") }),
         )
     }).bind(("127.0.0.1", 6969))?.run().await
 }
