7c0f82ee961d4bfd34bc117e27f67fbaed860d02
Filip Kovac <Filip.Kovac@adacta-fintech.com>
2023-11-17 18:17:05 +0100

    fix: updated match migration without odds

--- migrations/20231115154050_match.up.sql
+++ migrations/20231115154050_match.up.sql
@@ -1,1 +1,17 @@
--- Add up migration script here
+CREATE TYPE MatchStatus AS ENUM ('PENDING', 'LIVE', 'FINISHED', 'CANCELED');
+
+CREATE TABLE IF NOT EXISTS Match (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    ---------------------------------------------
+    name_a text NOT NULL,
+    name_b text NOT NULL,
+    starts_at timestamptz NOT NULL,
+    ends_at timestamptz NOT NULL,
+    status MatchStatus NOT NULL DEFAULT 'PENDING',
+    game_id uuid NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    edited_at timestamptz NOT NULL DEFAULT now(),
+    deleted_at timestamptz,
+    ----------------------------------------------
+    FOREIGN KEY (game_id) REFERENCES Game(id)
+);
