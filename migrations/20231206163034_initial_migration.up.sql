6cc78c26fb72f1f064902fa87707a569bfb8fe21
Ondřej Hložek <xhlozek@fi.muni.cz>
2023-12-06 19:09:46 +0100

    feat: add migration

---
+++ migrations/20231206163034_initial_migration.up.sql
@@ -0,0 +1,142 @@
+/* User table */
+CREATE TABLE IF NOT EXISTS AppUser (
+    id              uuid PRIMARY KEY     DEFAULT gen_random_uuid(),
+    ---------------------------------------------------------------
+    username        text        NOT NULL,
+    email           text        NOT NULL,
+    name            text        NOT NULL,
+    surname         text        NOT NULL,
+    profile_picture text        NOT NULL,
+    password_hash   text        NOT NULL,
+    password_salt   text        NOT NULL,
+    balance         int         NOT NULL,
+    created_at      timestamptz NOT NULL DEFAULT now(),
+    edited_at       timestamptz NOT NULL DEFAULT now(),
+    deleted_at      timestamptz
+);
+
+/* GameGenre enum */
+CREATE TYPE IF NOT EXISTS GameGenre AS ENUM (
+    'MOBA (MultiPlayer Online Battle Arena)',
+    'FPS (First Person Shooter)'
+);
+
+/* Game table */
+CREATE TABLE IF NOT EXISTS Game (
+    id          uuid PRIMARY KEY     DEFAULT gen_random_uuid(),
+    -----------------------------------------------------------
+    name        text        NOT NULL,
+    description text        NOT NULL,
+    logo        text        NOT NULL,
+    genre       GameGenre   NOT NULL,
+    created_at  timestamptz NOT NULL DEFAULT now(),
+    edited_at   timestamptz NOT NULL DEFAULT now(),
+    deleted_at  timestamptz
+)
+
+/* GameMatchStatus enum */
+CREATE TYPE IF NOT EXISTS GameMatchStatus AS ENUM (
+    'PENDING',
+    'LIVE',
+    'FINISHED',
+    'CANCELED'
+);
+
+/* GameMatchOutcome enum */
+CREATE TYPE IF NOT EXISTS GameMatchOutcome AS ENUM (
+    'WIN_A',
+    'WIN_B',
+    'DRAW'
+);
+
+/* GameMatch table */
+CREATE TABLE IF NOT EXISTS GameMatch (
+    id         uuid PRIMARY KEY          DEFAULT gen_random_uuid(),
+    ---------------------------------------------------------------
+    game_id    uuid             NOT NULL,
+    ---------------------------------------------------------------
+    name_a     text             NOT NULL,
+    name_b     text             NOT NULL,
+    starts_at  timestamptz      NOT NULL,
+    ends_at    timestamptz      NOT NULL,
+    status     GameMatchStatus  NOT NULL DEFAULT 'PENDING',
+    outcome    GameMatchOutcome,
+    created_at timestamptz      NOT NULL DEFAULT now(),
+    edited_at  timestamptz      NOT NULL DEFAULT now(),
+    deleted_at timestamptz,
+    ---------------------------------------------------------------
+    FOREIGN KEY (game_id) REFERENCES Game (id)
+);
+
+/* Odds table */
+CREATE TABLE IF NOT EXISTS Odds (
+    id            uuid PRIMARY KEY     DEFAULT gen_random_uuid(),
+    -------------------------------------------------------------
+    game_match_id uuid        NOT NULL,
+    -------------------------------------------------------------
+    odds_a        float       NOT NULL,
+    odds_b        float       NOT NULL,
+    created_at    timestamptz NOT NULL DEFAULT now(),
+    edited_at     timestamptz NOT NULL DEFAULT now(),
+    deleted_at    timestamptz,
+    -------------------------------------------------------------
+    FOREIGN KEY (game_match_id) REFERENCES GameMatch (id)
+);
+
+/* BetStatus enum */
+CREATE TYPE IF NOT EXISTS BetStatus AS ENUM (
+    'PENDING',
+    'WON',
+    'LOST',
+    'CANCELED'
+);
+
+/* Bet table */
+CREATE TABLE IF NOT EXISTS Bet (
+    id               uuid PRIMARY KEY          DEFAULT gen_random_uuid(),
+    ---------------------------------------------------------------------
+    app_user_id      uuid             NOT NULL,
+    game_match_id    uuid             NOT NULL,
+    ---------------------------------------------------------------------
+    amount           int              NOT NULL,
+    status           BetStatus        NOT NULL DEFAULT 'PENDING',
+    expected_outcome GameMatchOutcome NOT NULL,
+    created_at       timestamptz      NOT NULL DEFAULT now(),
+    edited_at        timestamptz      NOT NULL DEFAULT now(),
+    deleted_at       timestamptz,
+    ---------------------------------------------------------------------
+    FOREIGN KEY (app_user_id)   REFERENCES AppUser   (id),
+    FOREIGN KEY (game_match_id) REFERENCES GameMatch (id)
+)
+
+/* Currency enum */
+CREATE TYPE IF NOT EXISTS Currency AS ENUM (
+    'CZK',
+    'EUR',
+    'USD'
+);
+
+/* MoneyTransactionStatus enum */
+CREATE TYPE IF NOT EXISTS MoneyTransactionStatus AS ENUM (
+    'PENDING',
+    'COMPLETED',
+    'CANCELED'
+);
+
+/* MoneyTransaction table */
+CREATE TABLE IF NOT EXISTS MoneyTransaction (
+    id              uuid PRIMARY KEY                DEFAULT gen_random_uuid(),
+    --------------------------------------------------------------------------
+    app_user_id     uuid                   NOT NULL,
+    --------------------------------------------------------------------------
+    status          MoneyTransactionStatus NOT NULL DEFAULT 'PENDING',
+    amount_tokens   int                    NOT NULL,
+    amount_currency float                  NOT NULL,
+    currency        Currency               NOT NULL,
+    deposit         boolean                NOT NULL,
+    created_at      timestamptz            NOT NULL DEFAULT now(),
+    edited_at       timestamptz            NOT NULL DEFAULT now(),
+    deleted_at      timestamptz,
+    --------------------------------------------------------------------------
+    FOREIGN KEY (app_user_id) REFERENCES AppUser (id)
+);
