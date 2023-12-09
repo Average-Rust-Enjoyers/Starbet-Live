0d0d73891fcae77740d52b0b16ef12be0cf56d19
ava <67600061+adamvalt@users.noreply.github.com>
2023-12-09 15:30:47 +0100

    Model bet (#23)
    * feat: implement Bet model and related structs
    
    * feat: Added bet status to bet
    
    * feat: added outcome to game_match and bet
    
    ---------
    
    Co-authored-by: Filip Kovac <Filip.Kovac@adacta-fintech.com>

---
+++ src/models/bet.rs
@@ -0,0 +1,105 @@
+#![allow(dead_code)]
+
+use chrono::{DateTime, Utc};
+use uuid::Uuid;
+
+use super::game_match_outcome::GameMatchOutcome;
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum BetStatus {
+    Pending,
+    Won,
+    Lost,
+    Cancelled,
+}
+
+#[derive(sqlx::FromRow, Debug, PartialEq, Eq, Clone)]
+pub struct Bet {
+    pub id: Uuid,
+    // ----------
+    pub app_user_id: Uuid,
+    pub game_match_id: Uuid,
+    // ----------
+    pub amount: i32,
+    pub expected_outcome: GameMatchOutcome,
+    pub created_at: DateTime<Utc>,
+    pub edited_at: DateTime<Utc>,
+    pub deleted_at: Option<DateTime<Utc>>,
+    pub status: BetStatus,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct BetCreate {
+    pub id: Uuid,
+    pub app_user_id: Uuid,
+    pub game_match_id: Uuid,
+    pub amount: i32,
+    pub expected_outcome: GameMatchOutcome,
+}
+
+impl BetCreate {
+    pub fn new(
+        id: &Uuid,
+        app_user_id: &Uuid,
+        game_match_id: &Uuid,
+        amount: i32,
+        expected_outcome: GameMatchOutcome,
+    ) -> Self {
+        Self {
+            id: id.to_owned(),
+            app_user_id: app_user_id.to_owned(),
+            game_match_id: game_match_id.to_owned(),
+            amount,
+            expected_outcome,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct BetUpdate {
+    pub id: Uuid,
+    pub status: BetStatus,
+}
+
+impl BetUpdate {
+    pub fn new(id: &Uuid, status: BetStatus) -> Self {
+        Self {
+            id: id.to_owned(),
+            status,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct BetDelete {
+    pub id: Uuid,
+}
+
+impl BetDelete {
+    pub fn new(id: &Uuid) -> Self {
+        Self { id: *id }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct BetGetById {
+    pub id: Uuid,
+}
+
+impl BetGetById {
+    pub fn new(id: &Uuid) -> Self {
+        Self { id: *id }
+    }
+}
+
+impl From<&BetDelete> for BetGetById {
+    fn from(bet_delete: &BetDelete) -> Self {
+        Self { id: bet_delete.id }
+    }
+}
+
+impl From<&BetUpdate> for BetGetById {
+    fn from(bet_update: &BetUpdate) -> Self {
+        Self { id: bet_update.id }
+    }
+}
