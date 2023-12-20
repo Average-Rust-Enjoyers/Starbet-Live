a9a28321f9f78c233322423c5ffbc9f68e51a108
Jan Vaclav <79514386+vaclavj@users.noreply.github.com>
2023-12-20 15:50:12 +0100

    repositories/money_transaction: implement DbCreate, DbReadOne

--- src/repositories/money_transaction.rs
+++ src/repositories/money_transaction.rs
@@ -1,13 +1,16 @@
 #![allow(dead_code)]
-use sqlx::{Transaction, Postgres};
-use crate::models::money_transaction::{MoneyTransactionGetById, MoneyTransaction, MoneyTransactionStatus};
-use crate::common::error::DbResultSingle;
-
+use crate::common::error::BusinessLogicErrorKind;
+use crate::common::error::{BusinessLogicError, DbError, DbResultSingle};
+use crate::common::repository::{DbCreate, DbReadOne, PoolHandler};
+use crate::models::money_transaction::{
+    MoneyTransaction, MoneyTransactionCreate, MoneyTransactionGetById,
+};
+use async_trait::async_trait;
+use sqlx::{Acquire, Postgres, Transaction};
 
 pub struct MoneyTransactionRepository {
-    // pool_handler: PoolHandler,
+    pool_handler: PoolHandler,
 }
-
 
 impl MoneyTransactionRepository {
     /// Function which retrieves a single money transaction by its id, usable within a database transaction
@@ -23,13 +26,13 @@ impl MoneyTransactionRepository {
         params: MoneyTransactionGetById,
         transaction_handle: &mut Transaction<'a, Postgres>,
     ) -> DbResultSingle<Option<MoneyTransaction>> {
-        Ok(
-            sqlx::query_as!(MoneyTransaction,
-                r#"
+        Ok(sqlx::query_as!(
+            MoneyTransaction,
+            r#"
                     SELECT 
                         id,
                         app_user_id,
-                        status AS "status: MoneyTransactionStatus",
+                        status AS "status: _",
                         amount_tokens,
                         amount_currency,
                         currency AS "currency: _",
@@ -39,10 +42,91 @@ impl MoneyTransactionRepository {
                         deleted_at
                     FROM MoneyTransaction
                     WHERE id = $1
-                "#, params.id)
-                .fetch_optional(transaction_handle.as_mut())
-                .await?,
+                "#,
+            params.id
         )
+        .fetch_optional(transaction_handle.as_mut())
+        .await?)
     }
 
+    /// Function which checks if the money transaction is correct (existing and not deleted)
+    ///
+    /// # Params
+    /// - `transaction`: optional money transaction retrieved from the database
+    ///
+    /// # Returns
+    /// - `Ok(post)`: when the transaction exists and is not deleted
+    /// - `Err(DbError)`: with appropriate error description otherwise
+    pub(crate) fn is_transaction_correct(
+        transaction: Option<MoneyTransaction>,
+    ) -> DbResultSingle<MoneyTransaction> {
+        let Some(transaction) = transaction else {
+            return Err(DbError::from(BusinessLogicError {
+                error: BusinessLogicErrorKind::MoneyTransactionDoesNotExist,
+            }));
+        };
+
+        if transaction.deleted_at.is_some() {
+            return Err(DbError::from(BusinessLogicError {
+                error: BusinessLogicErrorKind::MoneyTransactionDoesNotExist,
+            }));
+        }
+
+        Ok(transaction)
+    }
+}
+
+#[async_trait]
+impl DbCreate<MoneyTransactionCreate, MoneyTransaction> for MoneyTransactionRepository {
+    async fn create(&mut self, data: &MoneyTransactionCreate) -> DbResultSingle<MoneyTransaction> {
+        let mut tx = self.pool_handler.pool.begin().await?;
+        // TODO: check if User exists and is not deleted
+
+        let conn = tx.acquire().await?;
+        Ok(sqlx::query_as!(
+            MoneyTransaction,
+            r#"
+                INSERT INTO MoneyTransaction
+                (
+                    app_user_id,
+                    amount_tokens,
+                    amount_currency,
+                    currency
+                )
+                VALUES ($1, $2, $3, $4)
+                RETURNING 
+                    id,
+                    app_user_id,
+                    status AS "status: _",
+                    amount_tokens,
+                    amount_currency,
+                    currency AS "currency: _",
+                    deposit,
+                    created_at,
+                    edited_at,
+                    deleted_at
+            "#,
+            data.app_user_id,
+            data.amount_tokens,
+            data.amount_currency,
+            data.currency as _
+        )
+        .fetch_one(conn)
+        .await?)
+    }
+}
+
+#[async_trait]
+impl DbReadOne<MoneyTransactionGetById, MoneyTransaction> for MoneyTransactionRepository {
+    async fn read_one(
+        &mut self,
+        params: &MoneyTransactionGetById,
+    ) -> DbResultSingle<MoneyTransaction> {
+        let mut tx = self.pool_handler.pool.begin().await?;
+
+        // TODO: check if user (transaction owner) exists and is not deleted
+        MoneyTransactionRepository::is_transaction_correct(
+            MoneyTransactionRepository::get_money_transaction(params.clone(), &mut tx).await?,
+        )
+    }
 }
