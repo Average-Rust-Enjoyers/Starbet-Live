a81d70e4fcdadd0368ab06e55c46bd17639f5f79
Adam Valt <valt.adam@gmail.com>
2023-12-20 15:44:06 +0100

    test: add database fixtures

---
+++ tests/fixtures/gamematch.sql
@@ -0,0 +1,180 @@
+INSERT INTO GameMatch (
+        id,
+        game_id,
+        name_a,
+        name_b,
+        starts_at,
+        ends_at,
+        status,
+        outcome
+    )
+VALUES (
+        '7e3eb6e6-1cca-46e3-aa12-4c6c887e046a',
+        'ead41a2d-5669-424b-9b55-1a23b1909159',
+        'Tawa',
+        'Moramanga',
+        '2023-10-29 02:55:13',
+        '2022-11-20 06:06:19',
+        'PENDING',
+        'WIN_A'
+    ),
+    (
+        '33b93500-a4ac-46fe-9b11-d3ebe78cbba1',
+        'ead41a2d-5669-424b-9b55-1a23b1909159',
+        'Kamiraba',
+        'Stephens Island',
+        '2023-02-23 05:34:16',
+        '2022-04-01 11:16:39',
+        'LIVE',
+        'WIN_A'
+    ),
+    (
+        'e15b40f4-0967-4b94-a043-bb34f331c46d',
+        'b6f1c213-30e5-4cac-9b05-e0a1befbe7ee',
+        'Wasum',
+        'Banda-Naira Island',
+        '2023-11-19 19:56:37',
+        '2022-09-27 22:14:29',
+        'FINISHED',
+        'WIN_A'
+    ),
+    (
+        '7bf063eb-40ae-4d99-a2b3-e2a3fe83e9b3',
+        'a3d487f8-2428-447c-af7b-03df0892b6ad',
+        'Archangelsk',
+        'Dabra-Papua Island',
+        '2022-12-23 00:23:19',
+        '2022-09-24 09:45:10',
+        'FINISHED',
+        'WIN_B'
+    ),
+    (
+        '8902ab54-61fd-40e3-808f-55ddfcd89e37',
+        'a3d487f8-2428-447c-af7b-03df0892b6ad',
+        'San Matías',
+        'Sevilla',
+        '2023-04-02 02:53:20',
+        '2023-01-16 12:35:49',
+        'PENDING',
+        'WIN_A'
+    ),
+    (
+        '1fcaa2dd-7e9b-4da1-af9c-d33105486c88',
+        'b6f1c213-30e5-4cac-9b05-e0a1befbe7ee',
+        'Anchorage',
+        'Suceava',
+        '2023-01-11 22:15:42',
+        '2022-11-16 06:29:51',
+        'LIVE',
+        'WIN_B'
+    ),
+    (
+        'de9e51a5-4f35-44c6-a9ee-7a3fb2fd1954',
+        '1f4642df-3d91-4303-902a-cec021694c13',
+        'Sialum',
+        'Oklahoma City',
+        '2023-09-05 10:29:35',
+        '2022-11-11 12:03:47',
+        'PENDING',
+        'WIN_A'
+    ),
+    (
+        'f4e4b046-906e-42af-a594-314c9c24e726',
+        'b6f1c213-30e5-4cac-9b05-e0a1befbe7ee',
+        'Salt Lake City',
+        'Bainbridge',
+        '2023-06-28 06:37:37',
+        '2022-05-03 02:25:28',
+        'FINISHED',
+        'WIN_B'
+    ),
+    (
+        '18e2bbf9-216b-4cbd-9829-583e75807ea3',
+        'ead41a2d-5669-424b-9b55-1a23b1909159',
+        'Khudzhand',
+        'Frederick',
+        '2023-01-14 08:13:09',
+        '2022-10-28 00:45:15',
+        'FINISHED',
+        'WIN_A'
+    ),
+    (
+        'cd2fce3e-7260-48be-9da1-b85df105e40a',
+        'b6f1c213-30e5-4cac-9b05-e0a1befbe7ee',
+        'Rockhampton',
+        'Mansa',
+        '2023-05-29 07:51:53',
+        '2022-10-07 19:19:54',
+        'FINISHED',
+        'WIN_A'
+    ),
+    (
+        '48717f54-370a-4fdb-846f-1261f2990336',
+        'a3d487f8-2428-447c-af7b-03df0892b6ad',
+        'Tokoname',
+        'Slave Lake',
+        '2023-10-12 13:34:16',
+        '2022-01-29 22:21:22',
+        'FINISHED',
+        'WIN_B'
+    ),
+    (
+        '3f329c46-62f5-4768-b4ac-1b79466527da',
+        'b6f1c213-30e5-4cac-9b05-e0a1befbe7ee',
+        'Muzaffarabad',
+        'Nunukan-Nunukan Island',
+        '2023-05-11 08:32:41',
+        '2022-03-15 06:40:56',
+        'FINISHED',
+        'WIN_B'
+    ),
+    (
+        'caa04ba2-974b-4c97-b145-4d94fb3acf0d',
+        'a3d487f8-2428-447c-af7b-03df0892b6ad',
+        'Flat',
+        'Madison',
+        '2023-01-23 03:34:04',
+        '2022-05-19 05:03:27',
+        'FINISHED',
+        'WIN_A'
+    ),
+    (
+        '1fea0853-502c-4a9c-99ac-4892b24741a9',
+        'b6f1c213-30e5-4cac-9b05-e0a1befbe7ee',
+        'Attawapiskat',
+        'Peawanuck',
+        '2023-10-08 16:45:27',
+        '2022-10-19 17:47:50',
+        'FINISHED',
+        'WIN_B'
+    ),
+    (
+        'b56c2fb1-6c6c-4931-97b2-525bf3970252',
+        '1f4642df-3d91-4303-902a-cec021694c13',
+        'Arua',
+        'Sorriso',
+        '2023-06-11 02:31:09',
+        '2022-08-14 04:01:33',
+        'FINISHED',
+        'DRAW'
+    ),
+    (
+        'b663689f-c825-44b7-ba1c-7ae6c6b8167c',
+        'ead41a2d-5669-424b-9b55-1a23b1909159',
+        'Penrhyn Island',
+        'Uribe',
+        '2023-02-24 07:55:26',
+        '2022-12-13 15:55:40',
+        'FINISHED',
+        'WIN_B'
+    ),
+    (
+        'b3fd8364-927c-4ec5-b846-68e624e7c98d',
+        'b6f1c213-30e5-4cac-9b05-e0a1befbe7ee',
+        'Ialibu',
+        'Cheyenne',
+        '2023-03-01 07:11:07',
+        '2022-07-06 06:41:24',
+        'PENDING',
+        'WIN_A'
+    );
