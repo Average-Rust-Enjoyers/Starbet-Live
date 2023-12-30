c8f8dd34feab472b08da1c828e8de3983114245f
Adam Valt <valt.adam@gmail.com>
2023-12-30 18:45:28 +0100

    db: rename game genre type to shorter name

---
+++ migrations/20231228081819_rename_game_genre_enum_values.up.sql
@@ -0,0 +1,4 @@
+ALTER TYPE GameGenre
+RENAME VALUE 'MOBA (MultiPlayer Online Battle Arena)' TO 'MOBA';
+ALTER TYPE GameGenre
+RENAME VALUE 'FPS (First Person Shooter)' TO 'FPS';
--- tests/fixtures/game.sql
+++ tests/fixtures/game.sql
@@ -1,29 +1,45 @@
-INSERT INTO Game (id, name, description, logo, genre)
+INSERT INTO Game (
+        id,
+        name,
+        description,
+        logo,
+        genre,
+        created_at,
+        edited_at
+    )
 VALUES (
         'b6f1c213-30e5-4cac-9b05-e0a1befbe7ee',
         'League of Legends',
         'League of Legends is a team-based game with over 140 champions to make epic plays with.',
         'https://gaming-cdn.com/images/products/9456/orig/league-of-legends-pc-game-cover.jpg?v=1662363312',
-        'MOBA (MultiPlayer Online Battle Arena)'
+        'MOBA',
+        '2023-12-09 19:38:46.728083+00:00',
+        '2023-12-09 19:38:46.728083+00:00'
     ),
     (
         'ead41a2d-5669-424b-9b55-1a23b1909159',
         'Counter-Strike 2',
         'Counter-Strike 2 expands upon the team-based action gameplay that it pioneered when it was launched 19 years ago.',
         'https://cdn.cloudflare.steamstatic.com/steam/apps/730/header.jpg?t=1607019058',
-        'FPS (First Person Shooter)'
+        'FPS',
+        '2023-12-09 19:38:46.728083+00:00',
+        '2023-12-09 19:38:46.728083+00:00'
     ),
     (
         '1f4642df-3d91-4303-902a-cec021694c13',
         'Dota 2',
         'Dota 2 is a multiplayer online battle arena (MOBA) video game developed and published by Valve.',
         'https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg?t=1607022750',
-        'MOBA (MultiPlayer Online Battle Arena)'
+        'MOBA',
+        '2023-12-09 19:38:46.728083+00:00',
+        '2023-12-09 19:38:46.728083+00:00'
     ),
     (
         'a3d487f8-2428-447c-af7b-03df0892b6ad',
         'Valorant',
         'Valorant is a free-to-play multiplayer tactical first-person shooter developed and published by Riot Games.',
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFpZfytSFnYi-3IfpNXsgvZKFUIuiA62PLaQ&usqp=CAU',
-        'FPS (First Person Shooter)'
+        'FPS',
+        '2023-12-09 19:38:46.728083+00:00',
+        '2023-12-09 19:38:46.728083+00:00'
     );
