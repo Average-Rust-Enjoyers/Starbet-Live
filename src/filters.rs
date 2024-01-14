dc748c54b341e5c7b6b65a87b7c2c473c95b1d98
Ondřej Hložek <xhlozek@fi.muni.cz>
2024-01-14 21:46:02 +0100

    feat: created custom askama filter

---
+++ src/filters.rs
@@ -0,0 +1,9 @@
+use askama::Result;
+use std::fmt::Display;
+
+// Custom Askama filter
+#[allow(clippy::unnecessary_wraps)]
+pub fn replace_underscores<T: Display>(s: T) -> Result<String> {
+    let s = s.to_string();
+    Ok(s.replace('_', " "))
+}
