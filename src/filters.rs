ddc967815ebf3e450e14b632974a6e830eba09fd
Ondřej Hložek <xhlozek@fi.muni.cz>
2024-01-16 00:00:03 +0100

    feat: change askama filter

--- src/filters.rs
+++ src/filters.rs
@@ -3,7 +3,16 @@ use std::fmt::Display;
 
 // Custom Askama filter
 #[allow(clippy::unnecessary_wraps)]
-pub fn replace_hyphens<T: Display>(s: T) -> Result<String> {
-    let s = s.to_string();
-    Ok(s.replace('-', " "))
+pub fn split_and_capitalize<T: Display>(s: T) -> Result<String> {
+    Ok(s.to_string()
+        .split('-')
+        .map(|word| {
+            let mut chars = word.chars();
+            match chars.next() {
+                None => String::new(),
+                Some(first_char) => first_char.to_uppercase().chain(chars).collect(),
+            }
+        })
+        .collect::<Vec<String>>()
+        .join(" "))
 }
