69c4307062187e76b1a311467986a01124f40013
Adam Valt <valt.adam@gmail.com>
2024-01-31 06:34:04 +0100

    build/fix: change wrong absolute path to relative

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     cynic_codegen::register_schema("cloudbet")
-        .from_sdl_file("/home/ava/Documents/fi/05s/pv281/Starbet-Live/src/schemas/cloudbet.graphql")
+        .from_sdl_file("./src/schemas/cloudbet.graphql")
         .unwrap()
         .as_default()
         .unwrap();
